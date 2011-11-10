99ac73e6933fab2e25ff5439bb4cd6efab11f057
jeffreywu <jeffreywu@pptv.com>
2011-11-10 03:59:31 +0000

    提交 TcpPeerPacket
    git-svn-id: http://svn01/repositories/r01/p2p/trunk@1336 b3a1389e-2159-5c43-9038-463bf877d0d9

---
+++ protocol/TcpPeerPacket.h
@@ -0,0 +1,239 @@
+#ifndef TCP_PEER_PACKET_H
+#define TCP_PEER_PACKET_H
+
+#include "boost/shared_ptr.hpp"
+
+namespace network
+{
+    class TcpConnection;
+}
+
+namespace protocol
+{
+    struct TcpCommonPacket
+        : Packet
+    {
+        template <typename Archive>
+        void serialize(Archive & ar)
+        {
+            Packet::serialize(ar);
+
+            ar & protocol_version_;
+        }
+
+        TcpCommonPacket()
+        {
+            protocol_version_ = PEER_VERSION;
+        }
+
+        boost::uint16_t protocol_version_;
+        boost::shared_ptr<network::TcpConnection> tcp_connection_;
+    };
+
+    struct TcpAnnounceRequestPacket
+        : PacketT<0xB0>
+        , TcpCommonPacket
+    {
+        template <typename Archive>
+        void serialize(Archive & ar)
+        {
+            TcpCommonPacket::serialize(ar);
+
+            ar & resource_id_;
+        }
+
+        TcpAnnounceRequestPacket()
+        {
+
+        }
+
+        TcpAnnounceRequestPacket(const RID & rid)
+            : resource_id_(rid)
+        {
+
+        }
+
+        RID resource_id_;
+    };
+
+    struct TcpAnnounceResponsePacket
+        : PacketT<0xB1>
+        , TcpCommonPacket
+    {
+        template <typename Archive>
+        void serialize(Archive & ar)
+        {
+            TcpCommonPacket::serialize(ar);
+
+            ar & resource_id_;
+            ar & peer_download_info_;
+            ar & reserve1_;
+            ar & reserve2_;
+            ar & reserve3_;
+            ar & block_map_;
+        }
+
+        TcpAnnounceResponsePacket()
+        {
+
+        }
+
+        TcpAnnounceResponsePacket(
+            boost::uint32_t transaction_id,
+            const RID & rid,
+            const PEER_DOWNLOAD_INFO & peer_download_info,
+            const BlockMap & block_map)
+            : resource_id_(rid)
+            , peer_download_info_(peer_download_info)
+            , block_map_(block_map)
+        {
+            transaction_id_ = transaction_id;
+        }
+
+        RID resource_id_;
+        PEER_DOWNLOAD_INFO peer_download_info_;
+        boost::uint8_t reserve1_;
+        boost::uint8_t reserve2_;
+        boost::uint8_t reserve3_;
+        BlockMap block_map_;
+    };
+
+    struct TcpSubPieceRequestPacket
+        : PacketT<0xB2>
+        , TcpCommonPacket
+    {
+        template <typename Archive>
+        void serialize(Archive & ar)
+        {
+            TcpCommonPacket::serialize(ar);
+
+            ar & resource_id_;
+            ar & util::serialization::make_sized<boost::uint16_t>(subpiece_infos_);
+            ar & priority_;
+        }
+
+        TcpSubPieceRequestPacket()
+        {
+
+        }
+
+        TcpSubPieceRequestPacket(
+            const RID & rid, 
+            const std::vector<SubPieceInfo> & subpiece_infos,
+            const boost::uint16_t priority)
+            : resource_id_(rid)
+            , subpiece_infos_(subpiece_infos)
+            , priority_(priority)
+        {
+
+        }
+
+        RID resource_id_;
+        std::vector<SubPieceInfo> subpiece_infos_;
+        boost::uint16_t priority_;
+    };
+
+    struct TcpSubPieceResponsePacket
+        : PacketT<0xB3>
+        , TcpCommonPacket
+    {
+        template <typename Archive>
+        void serialize(Archive & ar)
+        {
+            TcpCommonPacket::serialize(ar);
+
+            ar & resource_id_;
+            ar & sub_piece_info_;
+            ar & sub_piece_length_;
+
+            for (size_t i = 0 ; i < sub_piece_length_ ; i++)
+            {
+                ar & *(sub_piece_content_->get_buffer() + i);
+            }
+        }
+
+        TcpSubPieceResponsePacket()
+        {
+
+        }
+
+        TcpSubPieceResponsePacket(
+            boost::uint32_t transaction_id,
+            const RID & rid,
+            const SubPieceInfo & sub_piece_info,
+            SubPieceBuffer sub_piece_buffer)
+            : resource_id_(rid)
+            , sub_piece_info_(sub_piece_info)
+            , sub_piece_length_(sub_piece_buffer.Length())
+            , sub_piece_content_(sub_piece_buffer.GetSubPieceBuffer())
+        {
+            transaction_id_ = transaction_id;
+        }
+
+        RID resource_id_;
+        SubPieceInfo sub_piece_info_;
+        boost::uint16_t sub_piece_length_;
+        mutable SubPieceContent::pointer sub_piece_content_;
+    };
+
+    struct TcpReportSpeedPacket
+        : PacketT<0xB4>
+        , TcpCommonPacket
+    {
+        template <typename Archive>
+        void serialize(Archive & ar)
+        {
+            TcpCommonPacket::serialize(ar);
+
+            ar & speed_;
+        }
+
+        TcpReportSpeedPacket()
+        {
+
+        }
+
+        boost::uint32_t speed_;
+    };
+
+    struct TcpErrorPacket
+        : PacketT<0xB5>
+        , TcpCommonPacket
+    {
+        template <typename Archive>
+        void serialize(Archive & ar)
+        {
+            TcpCommonPacket::serialize(ar);
+
+            ar & error_code_;
+        }
+
+        TcpErrorPacket()
+        {
+
+        }
+
+
+        TcpErrorPacket(
+            const boost::uint32_t transaction_id,
+            const boost::uint16_t error_code_)
+            : error_code_(error_code_)
+        {
+            transaction_id_ = transaction_id;
+        }
+
+        boost::uint16_t error_code_;
+    };
+
+    struct TcpClosePacket
+        : PacketT<0xB6>
+        , TcpCommonPacket
+    {
+        TcpClosePacket()
+        {
+            PacketAction = 0xB6;
+        }
+    };
+}
+
+#endif
